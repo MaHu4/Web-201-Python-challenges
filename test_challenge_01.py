46cc97a77f0dfc2e6927cea833d77ca48fad584a
“MaHu4” <marenhuser@gmail.com>
2022-05-17 00:50:19 +0545

    modiefied test challenge 1

--- test_challenge_01.py
+++ test_challenge_01.py
@@ -4,7 +4,20 @@
 # should be counted as vowels — not y.
 
 def count_vowels(word):
-    pass
+    count = 0 #new variable starting from 0
+    for each_character in word:
+        if each_character == 'a' or each_character == 'e' or each_character == 'i' or each_character == 'o' or each_character == 'u': # : closes the if-statement
+            count = count + 1 
+    print(count)
+    #RETURN STATEMENT --> must be always the last line; ends the function
+    return count # returns count outside of the function to get the result; otherwise, the function returns NONE
+  
 
 def test_challenge_01_happy_case(): 
      assert count_vowels('Kaleidoscope') == 6   
+     # test needs to start with test_; afterwards new name can be given
+     #we can change the word Kaleidoscope by any other word
+
+# add test cases
+def test_emty_case(): 
+     assert count_vowels('') == 0 
