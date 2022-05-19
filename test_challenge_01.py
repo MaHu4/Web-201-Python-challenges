1c732821e487a65ff1ab367372dc62a104d534ef
“MaHu4” <marenhuser@gmail.com>
2022-05-19 16:59:35 +0545

    tests in challenge_01 added, all pf them passed successfully

--- test_challenge_01.py
+++ test_challenge_01.py
@@ -16,9 +16,25 @@ def count_vowels(word):
 def test_challenge_01_happy_case(): 
      assert count_vowels('Kaleidoscope') == 6   
      # test needs to start with test_; afterwards new test-name can be given
-    # each test-name must be unique
-     #we can change the word Kaleidoscope by any other word
+     # each test-name must be unique
+     # we can change the word Kaleidoscope by any other word
 
-# add test cases
+def test_challenge_02_happy_case(): 
+     assert count_vowels('Kundalini') == 4   
+     # other number of vowels --> passed
+ 
+def test_challenge_Y_case(): 
+     assert count_vowels('Yak') == 1   
+     # test that y is not counted as vowel --> passed
+
 def test_emty_case(): 
      assert count_vowels('') == 0 
+    # --> passed
+
+def test_no_vowel_case():
+    assert count_vowels('KTMND') == 0
+    # --> passed
+
+def test_numbers_as_string_case():
+    assert count_vowels('2022') == 0
+     # --> passed
