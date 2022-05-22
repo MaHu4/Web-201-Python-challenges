83c21c198a2c70d682adfeae809a58ae7fa7c2ff
“MaHu4” <marenhuser@gmail.com>
2022-05-22 11:52:59 +0530

    added first method in challene_04

--- test_challenge_04.py
+++ test_challenge_04.py
@@ -5,16 +5,31 @@
 # Write a function that takes an variable and returns 
 # True or False if the variable is a palindrome.
 
-# def is_palindrome(var):
-#     reverse = var.reverse()
 
-str = ["today"]
-str.reverse()
-print(str.reverse())
+# A string is a palindrome if the reverse of the string is the same as string.
 
 
-# def test_challenge_04_palindrome_number():
-#     assert is_palindrome(545) == True
+# Method 1: 
+    # Find reverse of string
+    # Check if reverse and original are same or not.
 
-# def test_challenge_04_palindrome_string():
-#     assert is_palindrome('MADAM') == True    
+def is_palindrome(var):
+    var = str("") # makes all var a string --> returns also integers into string  
+    return var == var[::-1]  # <object_name>[<start_index>, <stop_index>, <step>] -->  s[::-1] --> the object is going to slice every "step" index from the given start index, till the stop index (excluding the stop index) and returns it
+    
+# Method 2: 
+
+def test_challenge_04_palindrome_number():
+    assert is_palindrome(545) == True
+
+def test_challenge_04_palindrome_string():
+    assert is_palindrome('MADAM') == True  
+
+def test_challenge_04_palindrome_string2 ():
+    assert is_palindrome('madam') == True
+
+def test_challenge_04_palindrome_string3 ():
+    assert is_palindrome('MadaM') == True  
+
+def test_challenge_04_no_palindrome_string():
+    assert is_palindrome('maddam') == False  
