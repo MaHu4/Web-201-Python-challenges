084d76d37ff56b4181e41618c2553bc89cbed3e3
“MaHu4” <marenhuser@gmail.com>
2022-06-10 15:08:11 +0530

    started with challenge_07

--- test_challenge_07.py
+++ test_challenge_07.py
@@ -6,22 +6,42 @@
 # if total is greater than 70 Euros 7% discount and so on. 
 # --> for each 10€ = 1% discount starting from 50€
 
-def calculate_bill(shopping_list):
-    # if not isinstance(shopping_list, float) or not isinstance(shopping_list, int):
-    #     return "not calculable" 
+# def calculate_bill(shopping_list):
+#     # if not isinstance(shopping_list, float) or not isinstance(shopping_list, int):
+#     #     return "not calculable" 
     
-    Total = []  
+#     Total = 0
+#         for i in shopping_list:
+#         if isinstance(i, float):
+#             # Total = sum(shopping_list[i])
+#             Total = Total + i
+#     print(Total)
+    
+#     return(Total)
+
+def calculate_bill(shopping_list):
+    if not isinstance(shopping_list, float) or not isinstance(shopping_list, int):
+        return "not calculable"  
+    
+    sumTotal = {}
     for i in shopping_list:
         if isinstance(i, float):
-            Total = sum(shopping_list[i])
-    print(Total)
-    
-    return(Total)
+            sumTotal = sum(shopping_list[i])
+        
+    return(sumTotal)
 
-def test_challenge_07_happy_case(): 
+def test_challenge_07_happy_case_1 (): 
     shopping_list = {'apples':11.20, 'bananas':2.2, 'eggs':30.00}
     assert calculate_bill(shopping_list) == 43.4
 
-# def test_challenge_07_happy_case_2 (): 
-#     shopping_list = {'apples':11.20, 'bananas':2.2, 'eggs':30.00}
-#     assert calculate_bill(shopping_list) == 43.4
+def test_challenge_07_sad_not_calculable_case_1 (): 
+    shopping_list = {'apples': 'a', 'bananas':'b', 'eggs':'c'}
+    assert calculate_bill(shopping_list) == "not calculable"
+
+def test_challenge_07_sad_not_a_dictionary_case_1 ():  # is that test valid???
+    shopping_list = [11.20, 2.2, 20.00]
+    assert calculate_bill(shopping_list) == "not calculable" 
+
+def test_challenge_07_sad_not_a_dictionary_case_2 ():  # is that test valid???
+    shopping_list = (11.20, 2.2, 20.00)
+    assert calculate_bill(shopping_list) == "not calculable" 
